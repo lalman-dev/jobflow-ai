ce1ff5100a9d0a456c0cba7d5aee28ee58673e82
Lalman <lalmanchaudhary2@gmail.com>
2026-04-21 22:47:51 +0530

    feat(store): add update and delete actions for application CRUD

--- store/useApplicationStore.ts
+++ store/useApplicationStore.ts
@@ -3,14 +3,31 @@ import { Application } from "@/src/types/application";
 
 type AppState = {
   applications: Application[];
+
   addApplication: (app: Application) => void;
+
+  deleteApplication: (id: string) => void;
+
+  updateApplication: (updated: Application) => void;
 };
 
 export const useApplicationStore = create<AppState>((set) => ({
   applications: [],
-  
+
   addApplication: (app) =>
     set((state) => ({
       applications: [app, ...state.applications],
+    })),
+
+  deleteApplication: (id) =>
+    set((state) => ({
+      applications: state.applications.filter((app) => app.id !== id),
+    })),
+
+  updateApplication: (updated) =>
+    set((state) => ({
+      applications: state.applications.map((app) =>
+        app.id === updated.id ? updated : app,
+      ),
     })),
 }));
